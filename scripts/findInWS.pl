cfe66ff714ddcca252b6d29c7ad65f7aefc22075
Chirayu Shah <chirayu.shah14@gmail.com>
2016-02-04 22:27:37 +0100

    Updated the scripts

--- scripts/findInWS.pl
+++ scripts/findInWS.pl
@@ -6,14 +6,15 @@ use File::Find::Rule;
 
 #my $pathToSearch = $ENV{VC_WORKSPACE}/products/LodeRunner/LodeRunner/common
 #chdir(/Users\/chirayushah");
-my $pathToSearch = "/Users/chirayushah";
-my $inputDir = $ARGV[0];
-my $insideDir = $ARGV[1]; 
+my $pathToSearch = $ARGV[0];
+my $inputDir = $ARGV[1];
+my $insideDir = $ARGV[2]; 
 if (not defined $inputDir ) {
     die "I need some name to search";
 }
 #print "Dir 1: $inputDir, Dir 2  $insideDir"; 
 #print "You want to search $inputDir "; 
+
 my $rule = File::Find::Rule->new;
 $rule->directory;
 $rule->name( "*$inputDir*" );
@@ -21,17 +22,20 @@ $rule->maxdepth( 1 );
 my @dir = $rule->in( $pathToSearch );
 #print "Dir Parent: $dir[0]";
 foreach my $n (@dir) {
-#    print "$n \n";
-#    #chdir($n) or die "$!";
+    if (not defined $insideDir ) {
+        print "$n";
+    }
 }
-my $rule2 =  File::Find::Rule->new; 
-$rule2->directory; 
-$rule2->name( "*$insideDir*" );
-$rule2->maxdepth( 1 ); 
-my @insidedir = $rule2->in($dir[0]);
-foreach my $n (@insidedir) {
-    print "$n";
-    #chdir($n) or die "$!";
-} 
+if (defined $insideDir ) { 
+    my $rule2 =  File::Find::Rule->new; 
+    $rule2->directory; 
+    $rule2->name( "*$insideDir*" );
+    $rule2->maxdepth( 1 ); 
+    my @insidedir = $rule2->in($dir[0]);
+    foreach my $n (@insidedir) {
+        print "$n";
+        #chdir($n) or die "$!";
+    }
+}
 
 
--- scripts/perlwrapper.sh
+++ scripts/perlwrapper.sh
@@ -1,2 +1,84 @@
-#!/bin/bash
-cd $(perl findInWS.pl $1 $2)
+#!/bin/bash -e
+usage() {
+  case $1 in
+    1)
+       echo ""
+       echo "This script jumps to different locations in yous WorkSpace(WS)"
+       echo ""
+       echo "-----------------------------------------------------"
+       ;;
+    2)
+       echo "You must provide your new IP's name as a parameter."
+       echo "The IP name is any name that describes your "
+       echo ""
+       echo "Examples:"
+       echo "./create_ip.sh Radio, or ./create_ip.sh AnaDigTop, or  Usb, PathLogic, PowerManagement,"
+       echo "  UTxRxChain, etc"
+       echo ""
+       echo "-------------------------------------------------------"
+       ;;
+    3)
+       echo "After the IP has been added to your WS,"
+       echo "You can either checkin to SVN  at the same time"
+       echo "Or, you can add '-c' switch next time you run the script"
+       echo "IMPORTANT : This will ONLY perform checkin and update XML operation"
+       echo "            Assuming the IP has already been created first time without the switch"
+       echo ""
+       echo "Examples:"
+       echo "./create_ip.sh SpreadLeLong -c, or ./create_ip.sh Usb -c"
+       echo "NOTE : The Order is important and should be exactly like examples"
+       echo "---------------------------------------------------------------------"
+       ;;
+  esac
+}
+_vc=`eval echo $HOME`
+  if [ ! -d "$_vc" -o "$_vc" == "" ]; then
+    echo "ERROR - You have not set a correct VC_WORKSPACE"
+    echo "        Please correct this before trying again"
+    echo ""
+    #exit 1
+  fi
+ unset _vc 
+
+
+if [[ ${1:0:3} == "-ip" ]]; then
+    #go to ip folder
+    if [ "$#" -eq 2 ]; then
+        IPNAME=$2
+        #$PATHTOSEARCH="$VC_WORKSPACE/ip"
+        PATHTOSEARCH=$HOME
+        echo "finding IP $IPNAME in $PATHTOSEARCH"
+        cd $(perl findInWS.pl $PATHTOSEARCH $IPNAME) 
+    elif [ "$#" -eq 3 ]; then 
+        IPNAME=$2 
+        INSIDEIP=$3
+        echo "finding  $IPNAME/$INSIDEIP in $PATHTOSEARCH"
+        cd $(perl findInWS.pl $PATHTOSEARCH $IPNAME $INSIDEIP)
+    else
+        echo "Need atleast one argument"
+        usage 2
+        #exit 1
+    fi
+elif [[ ${1:0:3} == "-tb" ]]; then
+    #go to top level tb
+    echo "Bla"
+elif [[ ${1:0:3} == "-prod" ]]; then
+    #go to product folder
+    echo "Bla" 
+elif [[ ${1:0:3} == "-proj" ]]; then
+    #go to projects folder
+    echo "Bla" 
+elif [[ ${1:0:3} == "-method" ]]; then
+    #go to methodology folder
+    echo "Bla" 
+elif [[ ${1:0:3} == "-ext" ]]; then
+    #go to externals folder
+    echo "Bla" 
+else
+    usage 1
+    usage 2
+    usage 3
+    #exit 1
+fi
+
+#cd $(perl findInWS.pl $1 $2)
