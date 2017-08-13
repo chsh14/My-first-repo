3d4e1e19a27901dc8ff3586ccb8346395b83d5c4
chsh <chirayu.shah14@gmail.com>
2017-08-13 15:45:49 +0200

    Updates to monitor script

--- scripts/monitor_price.sh
+++ scripts/monitor_price.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -e
 
-URL_adidas=(http://www.adidas.no/terrex-swift-r-mid-gtx-shoes/BA9943.html http://www.adidas.com/us/terrex-swift-r-gtx-shoes/BZ0605.html)
-URL_nike=(https://store.nike.com/no/en_gb/pd/dry-academy-1-4-zip-football-drill-top/pid-11113012/pgid-11543879 https://store.nike.com/us/en_us/pd/dry-academy-mens-1-4-zip-soccer-drill-top/pid-11316902/pgid-11896250 https://store.nike.com/us/en_us/pd/air-max-infuriate-low-mens-basketball-shoe/pid-11596020/pgid-11621631)
+URL_adidas=(http://www.adidas.no/terrex-swift-r-mid-gtx-shoes/BA9943.html)
+URL_nike=(https://store.nike.com/no/en_gb/pd/dry-academy-1-4-zip-football-drill-top/pid-11113012/pgid-11543879 https://store.nike.com/us/en_us/pd/air-max-infuriate-low-mens-basketball-shoe/pid-11596020/pgid-11621631)
 
 
 
 check_adidas() {
     i=$1
-    printCust "Checking $i"
-    sale_price=$(curl -s "$i" | perl -nle 'print for m:data-sale-price=\"(.*)\"\s:')
-    standard_price_exists=$(curl -s "$i" | perl -nle 'print for m:data-standard-price=\"(.*)\":')
+    printCust "Checking $i"    
+    sale_price=$(curl  --connect-timeout 120 -s "$i" | perl -nle 'print for m:data-sale-price=\"(.*)\"\s:')
+    standard_price_exists=$(curl  -s "$i" --connect-timeout 120 | perl -nle 'print for m:data-standard-price=\"(.*)\":')
     #echo "standard_price_exists = ${standard_price_exists}"
     if [ ! -z ${standard_price_exists} ]; then
         standard_price=$(curl -s "$i" | perl -nle 'print for m:data-standard-price=\"(.*)\":')
         echo "$i is on SALE right now!!"
+        
         echo "item : $i , sale_price : $sale_price , standard_price : $standard_price " | mail -s "[chsh] SALE notification service " "chirayu.shah14@gmail.com"
         #send email about the sale
+    elif [[ ${sale_price} == "" ]]; then
+        echo "Item returned null.. Doesnt exist anymore" | mail -s "[chsh] SALE notification service " "chirayu.shah14@gmail.com"
     else
         echo "Current price for $i :  ${sale_price}"
     fi
@@ -25,20 +28,48 @@ check_adidas() {
 check_nike() {
     i=$1
     printCust "checking $i"
-    is_on_sale=$(curl -s "$i" | perl -nle 'print for m:span class.*product-on-sale:')
-    price=$(curl -s "$i" | perl -nle 'print for m:product\:price\:am.*content=\"(.*)\":')
+    price=$(curl --connect-timeout 120 -s "$i"  | perl -nle 'print for m:product\:price\:am.*content=\"(.*)\":')
+    #echo "$price"
+    is_on_sale=$(curl -s "$i" | perl -nle 'print for m:span class.*product-on-sale:')    
+    #status=$?
+    #echo "$is_on_sale , $status"
     #echo "price: $price"
     if [[ ${is_on_sale} == 1 ]]; then
         standard_price=$(curl -s "$i" | perl -nle 'print for m:overridden-local-price.*>(.*)<:')
         echo "$i is on SALE right now!!"
         echo "item : $i , sale_price : $price , standard_price : $standard_price " | mail -s "[chsh] SALE notification service " "chirayu.shah14@gmail.com"
-    else
+    elif [[  x${price} == "x" ]]; then
+        echo "Price Not exists.. removed?"
+        echo "Item returned null.. Doesnt exist anymore" | mail -s "[chsh] SALE notification service " "chirayu.shah14@gmail.com"
+    else         
         echo "Current price of $i: ${price}"
     fi
 
 }
 
-printCust () {
+
+check_global_sale() {
+    nike_us=https://www.nike.com/us/en_us/
+    nike_no=https://www.nike.com/no/en_gb/
+    global_us_sale_exists=$(curl -s "${nike_us}" | grep -ci "headline.*sale")
+    global_no_sale=$(curl -s "${nike_no}" | grep -ci "headline.*sale")
+    if [[ $global_us_sale_exists > 0 ]]; then
+        content=$(curl -s "${nike_us}" | grep -i "headline.*sale")
+        printCust "${nike_us}:""\n""$content" 
+    else
+        echo "No sale found"
+    fi
+    
+    if [[ $global_no_sale > 0 ]]; then
+        content=$(curl -s "${nike_no}" | grep -i "headline.*sale")
+        printCust "Nike content: \n $content"
+    else
+        printCust "${nike_no} No sale found.."
+    fi
+
+}
+
+printCust () {                               
 
  echo "----------------------------------------------------------"
  printf "%s\n" "$1"
@@ -55,9 +86,9 @@ printCust () {
 #    done
 #done
 
-URL+=("${URL_adidas[@]}" "${URL_nike[@]}")
+URL=("${URL_adidas[@]}" "${URL_nike[@]}")
 #declare -p URL
-
+check_global_sale
 for i in ${URL[@]}; do
     #echo "looking for $i"
     if [[ "$i" =~ .*nike\.com.* ]]; then
